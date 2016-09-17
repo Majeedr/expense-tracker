5c4c31e2e990101d879c072b783c841dd72d4eb1
Hernawan Fa'iz Abdillah <hernawan.faiz.a@mail.ugm.ac.id>
2016-09-17 22:03:42 +0700

    Change LICENSE notice

--- src/com/vinsol/expensetracker/helpers/FavoriteHelper.java
+++ src/com/vinsol/expensetracker/helpers/FavoriteHelper.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2012 Vinayak Solutions Private Limited 
- * See the file license.txt for copying permission.
+ * See the LICENSE file for copying permission.
  */
 
 package com.vinsol.expensetracker.helpers;
